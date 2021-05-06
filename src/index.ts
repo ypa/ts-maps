4cfa92fa615189b7d3b70bd00832d6a33302a431
Yan P Aung <yan.pye.aung@gmail.com>
2021-05-05 19:53:54 -0700

    Refactor using Mappable interface

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,6 @@ const user = new User();
 const company = new Company();
 const customMap = new CustomMap('map');
 
-customMap.addUserMarker(user);
-customMap.addCompanyMarker(company);
+customMap.addMarker(user);
+customMap.addMarker(company);
 
