a9530ad3e282c554cc4da1fe77cc9e9a05137b49
Yan P Aung <yan.pye.aung@gmail.com>
2021-05-06 20:21:53 -0700

    Add color property to Mappaple interface and have User and Company explicitly implement it

--- src/CustomMap.ts
+++ src/CustomMap.ts
@@ -1,10 +1,11 @@
-interface Mappable {
+export interface Mappable {
   location: {
     lat: number;
     lng: number;
   };
 
   markerContent(): string;
+  color: string;
 }
 
 export class CustomMap {
